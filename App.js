5e10d7debbc6ceebd84ab05ea1a3b1401231e50a
TheCubiq <jlajsek@gmail.com>
2022-07-01 00:00:18 +0200

    Demo: Scrolling Header

--- App.js
+++ App.js
@@ -1,53 +1,49 @@
-import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import Comp from "./components/Comp";
+import React, { useRef, useState, useEffect } from "react";
+import { Animated, Text, ScrollView, View } from "react-native";
 
-export default function App() {
-  const [counter, setCounter] = useState(0);
-
+export default () => {
+  const scrolling = useRef(new Animated.Value(0)).current;
+  const translation = scrolling.interpolate({
+    inputRange: [100, 200],
+    outputRange: [-100, 0],
+    extrapolate: "clamp"
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => alert("Hello World!")}
+    <>
+      <Animated.View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 80,
+          backgroundColor: "tomato",
+          transform: [
+            {
+              translateY: translation,
+            },
+          ],
+        }}
       >
-        <Text style={styles.btnText}>ahoj</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => setCounter(counter + 10)}
-      >
-        <Text style={styles.btnText}>+</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => setCounter(counter - 10)}
-      >
-        <Text style={styles.btnText}>-</Text>
-      </TouchableOpacity>
-      <Comp text={counter} />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text 
+            style={{
+              fontSize: 50,
+            }}
+          >Header</Text>
+        </View>
+      </Animated.View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  btn: {
-    margin: 10,
-    padding: 10,
-    borderRadius: 10,
-    backgroundColor: "#ddd",
-    color: "#000",
-  },
-  btnText: {
-    fontSize: 20,
-  },
-});
+      <Animated.ScrollView
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrolling } } }],
+          { useNativeDriver: true }
+        )}
+        scrollEventThrottle={16}
+        style={{ flex: 1 }}
+      >
+        <View style={{ flex: 1, height: 1500 }} />
+      </Animated.ScrollView>
+    </>
+  );
+};

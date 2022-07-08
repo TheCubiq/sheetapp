79faf846c1e39c2b5dcd8dacd68227e6ec88a91d
TheCubiq <jlajsek@gmail.com>
2022-07-08 23:04:59 +0200

    demo view with a button that toggles the bottom sheet

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React from "react";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import {
-  Button,
-  View,
   StyleSheet,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  Alert,
+  Platform,
 } from "react-native";
-import {
-  GestureHandlerRootView,
-} from "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import MyBottomSheet2 from "./components/MyBottomSheet2";
-
-
+import MyBottomSheet from "./components/MyBottomSheet";
 
 export default () => {
-  const [showsheet, setShowSheet] = React.useState(false);
+  const [showsheet, setShowSheet] = useState(false);
   return (
     <>
       <GestureHandlerRootView
@@ -21,18 +21,50 @@ export default () => {
           flex: 1,
         }}
       >
-        <View style={styles.container}>
-          
-          <Button
-            title="Open sheet"
-            color={"#b103b4"}
-
+        <StatusBar hidden={true} />
+        <SafeAreaView style={styles.container}>
+          <TouchableOpacity
+            style={styles.btn}
             onPress={() => {
-              setShowSheet(true);
+              setShowSheet(!showsheet);
             }}
-          />
-        </View>
-        <MyBottomSheet2 show={showsheet}/>
+          >
+            <Text style={styles.btnText}>ToggleBottomSheet</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
+        <MyBottomSheet show={showsheet}>
+          <TouchableOpacity
+            onPress={() => {
+              console.log("ily");
+              Alert.alert(
+                "ily",
+                "I Love Ya",
+                [
+                  {
+                    text: "Ok lol",
+                    style: "cancel",
+                  },
+                  {
+                    text: "No ya don't",
+                    onPress: () => alert("Yes I do"),
+                  },
+                  { text: "LY Too" },
+                ],
+                { cancelable: false }
+              );
+            }}
+          >
+            <Text
+              style={{
+                color: "#ffffff",
+                fontSize: 50,
+                fontWeight: "bold",
+              }}
+            >
+              Click me
+            </Text>
+          </TouchableOpacity>
+        </MyBottomSheet>
       </GestureHandlerRootView>
     </>
   );
@@ -42,11 +74,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
+    backgroundColor: "#131313",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
   },
   btn: {
-    padding: 10,
-    margin: 10,
+    backgroundColor: "#b375e2ff",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "50%",
+    marginTop: 20,
     borderRadius: 10,
+    padding: 10,
+  },
+  btnText: {
+    fontSize: 24,
+    color: "#fff",
   },
 });
